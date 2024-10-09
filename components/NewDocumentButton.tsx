30689d50b5d3bebbe7bfebb460ff70a1a0e2998a
Jonathan Perry <jpper@umich.edu>
2024-10-09 01:36:52 -0500

    Implement the create a new document functionality

--- components/NewDocumentButton.tsx
+++ components/NewDocumentButton.tsx
@@ -1,6 +1,27 @@
+"use client";
+
+import { useTransition } from "react";
 import { Button } from "./ui/button";
+import { useRouter } from "next/navigation";
+import { createNewDocument } from "@/actions/actions";
 
 const NewDocumentButton = () => {
-  return <Button>New Document</Button>;
+  const [isPending, startTransition] = useTransition();
+  const router = useRouter();
+
+  const handleCreateNewDocument = () => {
+    startTransition(async () => {
+      // Create new doc
+      const { docId } = await createNewDocument();
+
+      router.push(`/doc/${docId}`);
+    });
+  };
+  return (
+    <Button onClick={handleCreateNewDocument} disabled={isPending}>
+      New Document
+    </Button>
+  );
 };
+
 export default NewDocumentButton;
