148942faa6a31fcce10007cc4bfc8a1a9a036175
Jonathan Perry <jpper@umich.edu>
2024-10-09 13:39:06 -0500

    Built the Document component

--- components/Document.tsx
+++ components/Document.tsx
@@ -1,6 +1,62 @@
-const Document = () => {
+"use client";
+
+import { FormEvent, useEffect, useState, useTransition } from "react";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "@/firebase";
+import { useDocumentData } from "react-firebase-hooks/firestore";
+
+const Document = ({ id }: { id: string }) => {
+  const [data, loading, error] = useDocumentData(doc(db, "documents", id));
+  const [input, setInput] = useState("");
+  const [isUpdating, startTransition] = useTransition();
+
+  useEffect(() => {
+    if (data) {
+      setInput(data.title);
+    }
+  }, [data]);
+
+  const updateTitle = (e: FormEvent) => {
+    e.preventDefault();
+
+    if (input.trim()) {
+      startTransition(async () => {
+        await updateDoc(doc(db, "documents", id), {
+          title: input,
+        });
+      });
+    }
+  };
+
   return (
-    <div>Document</div>
-  )
-}
-export default Document
+    <div>
+      <div className="flex max-w-6xl mx-auto justify-between pb-5">
+        <form className="flex flex-1 space-x-2" onSubmit={updateTitle}>
+          {/* Update title */}
+          <Input
+            value={input}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setInput(e.target.value)
+            } // Add proper type annotation for event
+          />
+
+          <Button disabled={isUpdating} type="submit">
+            {isUpdating ? "Updating..." : "Update"}
+          </Button>
+
+          {/* If owner && InviteUser, Delete Document */}
+        </form>
+      </div>
+      <div>
+        {/* Manage Users */}
+
+        {/* Avatars */}
+      </div>
+
+      {/* Collaborative Editor */}
+    </div>
+  );
+};
+export default Document;
