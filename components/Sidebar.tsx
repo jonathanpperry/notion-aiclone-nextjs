0c11025f666c1e97f5363259d6bd5a583fcdcfbe
Jonathan Perry <jpper@umich.edu>
2024-10-09 09:57:22 -0500

    Display all documents in Sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,21 +1,101 @@
+"use client";
 import { MenuIcon } from "lucide-react";
 import NewDocumentButton from "./NewDocumentButton";
+import { useCollection } from "react-firebase-hooks/firestore";
 import {
   Sheet,
   SheetContent,
-  SheetDescription,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import { useUser } from "@clerk/nextjs";
+import {
+  collectionGroup,
+  DocumentData,
+  query,
+  where,
+} from "firebase/firestore";
+import { db } from "@/firebase";
+import { useEffect, useState } from "react";
+
+interface RoomDocument extends DocumentData {
+  createdAt: string;
+  role: "owner" | "editor";
+  roomId: string;
+  userId: string;
+}
 
 const Sidebar = () => {
+  const { user } = useUser();
+  const [groupedData, setGroupedData] = useState<{
+    owner: RoomDocument[];
+    editor: RoomDocument[];
+  }>({
+    owner: [],
+    editor: [],
+  });
+
+  const [data, loading, error] = useCollection(
+    user &&
+      query(
+        collectionGroup(db, "rooms"),
+        where("userId", "==", user.emailAddresses[0].toString())
+      )
+  );
+
+  useEffect(() => {
+    if (!data) return;
+
+    const grouped = data.docs.reduce<{
+      owner: RoomDocument[];
+      editor: RoomDocument[];
+    }>(
+      (acc, cur) => {
+        const roomData = cur.data() as RoomDocument;
+
+        if (roomData.role === "owner") {
+          acc.owner.push({
+            id: cur.id,
+            ...roomData,
+          });
+        } else {
+          acc.editor.push({
+            id: cur.id,
+            ...roomData,
+          });
+        }
+        return acc;
+      },
+      {
+        owner: [],
+        editor: [],
+      }
+    );
+    setGroupedData(grouped);
+  }, [data]);
+
   const menuOptions = (
     <>
       <NewDocumentButton />
 
-      {/* My Documents */}
-      {/* List... */}
+      <div className="flex py-4 flex-col space-y-4 md:max-w-36">
+        {/* My Documents */}
+        {groupedData.owner.length === 0 ? (
+          <h2 className="text-gray-500 font-semibold text-sm">
+            No documents found
+          </h2>
+        ) : (
+          <>
+            <h2 className="text-gray-500 font-semibold text-sm">
+              My Documents
+            </h2>
+            {groupedData.owner.map((doc) => (
+              <p>{doc.roomId}</p>
+            ))}
+          </>
+        )}
+      </div>
 
       {/* Shared with me */}
       {/* List... */}
