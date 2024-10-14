d8920a3c697a3bf2229adb5da7c21e62aec1fc26
Jonathan Perry <jpper@umich.edu>
2024-10-14 00:15:24 -0500

    Implemented cloudflare workers AI with hono and implemented openai api for the document translation functionality

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -11,6 +11,7 @@ import { useCreateBlockNote } from "@blocknote/react";
 import "@blocknote/core/fonts/inter.css";
 import "@blocknote/shadcn/style.css";
 import stringToColor from "@/lib/stringToColor";
+import TranslateDocument from "./TranslateDocument";
 
 type EditorProps = {
   doc: Y.Doc;
@@ -75,7 +76,8 @@ const Editor = () => {
     <div className="max-w-6xl mx-auto">
       <div className="flex items-center gap-2 justify-end mb-10">
         {/* TranslateDocument AI */}
-        {/* TranslateDocument AI */}
+        <TranslateDocument doc={doc} />
+        {/* ChatToDocument AI */}
 
         {/* Dark Mode */}
         <Button className={style} onClick={() => setDarkMode(!darkMode)}>
